c93fdb0d10cf4febdd7252b5efa8b9d7e6464423
Poblete999 <129112124+Poblete999@users.noreply.github.com>
2023-05-17 21:10:21 -0400

    Add files via upload

---
+++ Ejercicio1.py
@@ -0,0 +1,27 @@
+
+
+Superficie8va = 23890
+Superficie10ma = 48583
+Habitantes8va = 1556805
+Habitantes10ma = 828708
+
+
+
+Dic = {"8va Región" : "Bio Bio",
+       "10ma Región" : "Los lagos",
+       "Superficie 8va región" : Superficie8va,
+       "Superficie 10ma región" : Superficie10ma, 
+       "Habitantes 8va región" : Habitantes8va,
+       "Habitantes 10ma región" : Habitantes10ma
+}
+
+
+print(Dic)
+
+Densidad8va = round(Habitantes8va/Superficie8va,1)
+Densidad10ma = round(Habitantes10ma/Superficie10ma,1)
+
+print(Densidad8va)
+print(Densidad10ma)
+
+
---
+++ Ejercicio2.py
@@ -0,0 +1,14 @@
+a = [10,9,12,15,18]
+b = [21,8,15,3,12]
+
+ListaA = a + b
+print(ListaA)
+
+ListaA.insert(1,30)
+print(ListaA)
+
+
+ListaB = [4,5,6]
+
+ListaA.append(ListaB)
+print(ListaA)
